6d97bbf9ae676167d6a6dc56c9598dba4ebc654f
WishNyaSydorenkoO <mobile365ua@gmail.com>
2022-07-25 23:07:23 +0300

    1-9

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,17 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const refs = {
+  bodyRef: document.body,
+  changeButtonRef: document.querySelector(".change-color"),
+  backgroundColorRef: document.querySelector(".color"),
+};
+
+function render() {
+  const color = getRandomHexColor();
+  refs.bodyRef.style.backgroundColor = color;
+  refs.backgroundColorRef.textContent = color;
+}
+
+refs.changeButtonRef.addEventListener("click", render);
--- js/task-10.js
+++ js/task-10.js
@@ -1,3 +1,4 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+// Проверите это задание, пожалуйста, если сдам  вместе со следующим Д\З ?
